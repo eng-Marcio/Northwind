38d7109dbb732b6bfb22ff5da1edeab80778f64d
eng-Marcio <marcio37nandi@gmail.com>
2021-01-27 01:16:23 -0300

    Finished producing charts

--- DBReader.py
+++ DBReader.py
@@ -1,6 +1,7 @@
 from os import listdir
 from os import path
 import numpy as np
+from numpy.core.fromnumeric import argsort
 from numpy.lib.index_tricks import CClass
 import pandas as pd
 
@@ -10,6 +11,8 @@ class DBReader:
         self.dataSet = []
         self.readCSVs()
         self.createAdditionals()
+        self.calculateCustomerData()
+        self.calculateChurn()
         
 
     def readCSVs(self):
@@ -56,7 +59,7 @@ class DBReader:
         self.dataSet.append(["orders_total_price", val_per_order])
 
         ###TBO: time between orders: the interval through which the customer makes an order
-        order_date = self.toNumber(self.getDataByName("orders_order_date"))
+        order_date = self.dateToCount(self.getDataByName("orders_order_date"))
         order_customer = self.getDataByName("orders_customer_id")
 
         sorter = np.argsort(order_customer.reshape(-1), kind='stable')
@@ -90,21 +93,149 @@ class DBReader:
         tbo_all_dates = tbo_all_dates[sorter]
         tbo_all = tbo_all[sorter]
         
-        self.dataSet.append(["utils_tbo_dates", tbo_all_dates])
-        self.dataSet.append(["utils_tbo", tbo_all])
+        self.dataSet.append(["utils_tbo_dates", tbo_all_dates.reshape([-1,1])])
+        self.dataSet.append(["utils_tbo", tbo_all.reshape([-1,1])])
         
-        #create a tbo collumn for cutomer table
-        c_c_id = self.getDataByName("customers_customer_id")
-        tab = np.empty(c_c_id.shape)
-        for i in range(len(checked_customers)):
-            tab[np.where(c_c_id == checked_customers[i])] = tbo_customer[i]
-        self.dataSet.append(["customers_tbo", tab])
+        ### calculate and add TTS: time to ship the orders given from the customer
+        shipdate = self.getDataByName("orders_shipped_date")
+        orderdate = self.getDataByName("orders_order_date")
+        requireddate = self.getDataByName("orders_required_date")
+        order0 = orderdate[0][0]
+        shipdate = self.dateToCount(shipdate, order0)
+        orderdate = self.dateToCount(orderdate, order0)
+        requireddate = self.dateToCount(requireddate, order0)
+        orders_delayed = (shipdate.reshape(-1)>requireddate.reshape(-1)).reshape([-1,1])
+        
+        #delete where products were not shipped yet
+        valids = np.where(shipdate > 0)
+        shipdate = shipdate[valids].reshape([-1,1])
+        orderdate = orderdate[valids].reshape([-1,1])
+        requireddate = requireddate[valids].reshape([-1,1])
+        date_set = self.getDataByName("orders_order_date")[valids]
+        orders_delayed = orders_delayed[valids]
+
+        abs_time = shipdate - orderdate
+        relative_time = np.divide((shipdate - orderdate),(requireddate - orderdate))*100 #percentage of delivery time took to send the product
+        self.dataSet.append(["utils_tts_dates", date_set.reshape([-1,1])])
+        self.dataSet.append(["utils_tts", abs_time])
+        self.dataSet.append(["utils_tts_relative", relative_time])
+        self.dataSet.append(["utils_tts_delay", orders_delayed.reshape([-1,1])])
+
+
+    def calculateCustomerData(self):
+        first_date = self.getDataByName("orders_order_date")[0][0]
+
+        order_customer = self.getDataByName("orders_customer_id").reshape(-1)
+        order_prices = self.getDataByName("orders_total_price").reshape(-1)
+        order_dates = self.dateToCount(self.getDataByName("orders_order_date").reshape(-1), first_date)
+        order_ship_date = self.dateToCount(self.getDataByName("orders_shipped_date").reshape(-1), first_date)
+        order_required_date = self.dateToCount(self.getDataByName("orders_required_date").reshape(-1), first_date)
+        customer_id = self.getDataByName("customers_customer_id").reshape(-1)
+        
+        
+        customer_average_ticket = np.empty(customer_id.shape[0])
+        customer_delta_ticket = np.empty(customer_id.shape[0])
+        customer_tbo = np.empty(customer_id.shape[0])
+        customer_delta_tbo = np.empty(customer_id.shape[0])
+        customer_delay_rate = np.empty(customer_id.shape[0])
+        customer_tts = np.empty(customer_id.shape[0])
+        
+        customer_churn = np.empty(customer_id.shape[0])
+        last_day = order_dates[-1]
+
+        for i in range(customer_id.shape[0]):
+            customer_orders = np.where(order_customer == customer_id[i])
+            dates = order_dates[customer_orders]
+            if(dates.shape[0] < 2):
+                customer_average_ticket[i] = 0
+                customer_delta_ticket[i] = 0
+                customer_tbo[i] = 0
+                customer_delta_tbo[i] = 0
+                customer_delay_rate[i] = 0
+                customer_tts[i] = 0
+                customer_churn[i] = 1
+                continue
+            ship_date = order_ship_date[customer_orders]
+            required_date = order_required_date[customer_orders]
+            intervals = np.diff(dates)
+            values = order_prices[customer_orders]
+
+            tbo = np.mean(intervals)
+            delta_tbo = np.mean(np.diff(intervals))
+            c_ticket = np.mean(values)
+            c_delta_ticket = np.mean(np.diff(values))
+            delays = np.sum(ship_date > required_date)
+            
+            shipped = np.where(ship_date > 0)
+            tts = np.mean(ship_date[shipped] - dates[shipped])
+
+            if (intervals.shape[0] == 0):
+                churn_rate = 1
+            else:
+                churn_rate = (intervals[-1] + last_day - dates[-1]) / (2* tbo + np.finfo(float).eps)
+            
+            customer_average_ticket[i] = c_ticket
+            customer_delta_ticket[i] = c_delta_ticket
+            customer_tbo[i] = tbo
+            customer_delta_tbo[i] = delta_tbo
+            customer_delay_rate[i] = delays / (float(dates.shape[0]) + np.finfo(float).eps)
+            customer_tts[i] = tts
+            customer_churn[i] = churn_rate
+
+        self.dataSet.append(["customers_avg_order", customer_average_ticket])
+        self.dataSet.append(["customers_delta_order", customer_delta_ticket])
+        self.dataSet.append(["customers_tbo", customer_tbo])
+        self.dataSet.append(["customers_delta_tbo", customer_delta_tbo])
+        self.dataSet.append(["customers_delay_rate", customer_delay_rate])
+        self.dataSet.append(["customers_tts", customer_tts])
+        self.dataSet.append(["customers_churn_rate", customer_churn])
+        
+            
+
 
 
 
         
-    def toNumber(self, array):
-        x = array - np.min(array)
+
+    
+
+    def calculateChurn(self):
+        customer_id = self.getDataByName("customers_customer_id")
+        tbo_costumer = self.getDataByName("customers_tbo")
+        orders_ct = self.getDataByName("orders_customer_id")
+        order_date = self.dateToCount(self.getDataByName("orders_order_date")).reshape(-1)
+        
+        last_orders = np.empty(customer_id.shape[0])
+        first_orders = np.full(customer_id.shape[0], -100)
+        #get 2 last orders of each customers
+        for i in range(order_date.shape[0]):
+            index = np.where(customer_id == orders_ct[i])[0]
+            if(first_orders[index] == -100):
+                first_orders[index] = order_date[i]
+            last_orders[index] = order_date[i]
+        
+        #condition created to determine churn: if last order + 1.5*TBO < current date
+        last_date = order_date[-1]
+        churn, = np.where((last_orders + (1.5 * tbo_costumer.reshape(-1))) < last_date)
+        start_date = self.getDataByName("orders_order_date")[0][0]
+        churn_date = np.full(customer_id.shape[0], 0, dtype='datetime64[D]')
+        firts_date = np.full(customer_id.shape[0], 0, dtype='datetime64[D]')
+        for i in churn:
+            churn_date[i] = start_date + np.timedelta64(int(last_orders[i]), 'D')
+        for i in range(firts_date.shape[0]):
+            firts_date[i] = start_date + np.timedelta64(int(first_orders[i]), 'D')
+        
+        self.dataSet.append(["customers_churn", churn_date])
+        self.dataSet.append(["customers_first_order", firts_date])
+
+
+    def dateToCount(*args):
+        array = args[1]
+        if(len(args)==2):
+            reference = args[1][0]
+        else:
+            reference = args[2]
+        x = array - reference
         return x.astype('int32')
 
         
--- simple_drawer.py
+++ simple_drawer.py
@@ -7,22 +7,122 @@ import numpy as np
 class SimpleDrawer:
     def __init__(self, dataset):
         self.dataset = dataset
-        #self.drawMeanTicket()
-        #self.drawNrOrders()
-        #self.drawProfit()
+        # self.drawMeanTicket()
+        # self.drawNrOrders()
+        # self.drawProfit()
+        # self.drawTBO()
+        # self.drawTTS()
+        # self.drawChurn()
+
+        # self.drawHistogram(self.dataset.getDataByName("utils_tbo"), title="Histogram TBO", pic_name="hist_tbo")
+        # self.drawHistogram(self.dataset.getDataByName("utils_tts"), title="Histogram TTS", pic_name="hist_tts")
+        # self.drawHistogram(self.dataset.getDataByName("customers_tbo"), title="Histogram TBO-Cliente", pic_name="hist_client_tbo")
+        # self.drawHistogram(self.dataset.getDataByName("orders_total_price"), title="Histogram Ticket", pic_name="hist_price")
+
+        #self.drawTopProducts()
+        self.draw_category()
+
+    def draw_category(self):
+        product = self.dataset.getDataByName("order_details_product_id")
+        p_prod_id = self.dataset.getDataByName("products_product_id")
+        p_cat_id = self.dataset.getDataByName("products_category_id")
+
+        c_cat_id = self.dataset.getDataByName("categories_category_id")
+        c_cat_name = self.dataset.getDataByName("categories_category_name")
+        
+        cat_id_res = []
+        for i in product:
+            cat_id = p_cat_id[np.where(p_prod_id == i)[0]]
+            cat_id_res.append(cat_id)
+        cat_id_res = np.array(cat_id_res).reshape([-1,1])
+
+        quantity = self.dataset.getDataByName("order_details_quantity")
+        u_price = self.dataset.getDataByName("order_details_unit_price")
+        discount = self.dataset.getDataByName("order_details_discount")
+
+        raw_values = u_price * quantity * (1 - discount)
+        product_quantity = pd.DataFrame(np.concatenate([cat_id_res, quantity], axis=1)).groupby(0, as_index=False).sum().to_numpy()
+        product_value = pd.DataFrame(np.concatenate([cat_id_res, raw_values], axis=1)).groupby(0, as_index=False).sum().to_numpy()
+
+        label_q = []
+        label_v = []
+        for i in range(product_quantity.shape[0]):
+            label_q.append(c_cat_name[np.where(c_cat_id == product_quantity[i][0])[0]])
+            label_v.append(c_cat_name[np.where(c_cat_id == product_value[i][0])[0]])
+        label_q = np.array(label_q).reshape(-1)
+        label_v = np.array(label_v).reshape(-1)
+        self.drawPie(label_q, product_quantity[:, 1])
+        self.drawPie(label_v, product_value[:, 1])
+
+
+    def drawTopProducts(self):
+        product = self.dataset.getDataByName("order_details_product_id")
+        quantity = self.dataset.getDataByName("order_details_quantity")
+        u_price = self.dataset.getDataByName("order_details_unit_price")
+        discount = self.dataset.getDataByName("order_details_discount")
+
+        raw_values = u_price * quantity * (1 - discount)
+        product_quantity = pd.DataFrame(np.concatenate([product, quantity], axis=1)).groupby(0, as_index=False).sum().to_numpy()
+        product_value = pd.DataFrame(np.concatenate([product, raw_values], axis=1)).groupby(0, as_index=False).sum().to_numpy()
+        
+        sorter = np.argsort(product_quantity[:,1])
+        product_quantity = product_quantity[np.flip(sorter)]
+        sorter = np.argsort(product_value[:,1])
+        product_value = product_value[np.flip(sorter)]
+
+
+        p_prod_id = self.dataset.getDataByName("products_product_id")
+        names = self.dataset.getDataByName("products_product_name")
+
+        ###draw only the 20 most used products, and top 20 products in values sold
+        quantities = product_quantity[:20,1]
+        values = product_value[:20,1]
+        
+        labels_q = []
+        labels_v = []
+        for i in range(20):
+            index = np.where(p_prod_id == product_quantity[i][0])
+            labels_q.append(names[index][0])
+            index = np.where(p_prod_id == product_value[i][0])
+            labels_v.append(names[index][0])
+
+        self.drawColumn(labels_q,quantities)
+        self.drawColumn(labels_v,values)
+
+
+    def drawColumn(self, labels, values, labelX="interval", labelY="frequency", title="", pic_name=""):
+        y_pos = np.arange(len(labels))
+
+        plt.bar(y_pos, values, align='center', alpha=0.5)
+        plt.xticks(y_pos, labels, rotation=90)
+        plt.ylabel(labelY)
+        plt.title(title)
+        plt.gcf().subplots_adjust(bottom=0.5)
+        if(pic_name != ""):
+            plt.savefig(path.join("./charts", pic_name + ".png"))
+        plt.show()
+
+    def drawPie(self, labels, sizes):
+
+        fig1, ax1 = plt.subplots()
+        ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
+                shadow=True, startangle=90)
+        ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+        plt.show()
+        
     
     def drawMeanTicket(self):
         orderdate = self.dataset.getDataByName("orders_order_date")
         val_per_order = self.dataset.getDataByName("orders_total_price") 
        
-        month_of_orders = self.monthToInt(self.ignoreDays(orderdate))
+        month_of_orders = self.dateToCount(self.ignoreDays(orderdate))
         val_per_order = pd.DataFrame(np.concatenate([month_of_orders, val_per_order], axis=1)).groupby(0).mean().to_numpy() #mean ticket per month
         date_to_plot = np.unique(self.ignoreDays(orderdate))
         self.drawLine(date_to_plot[1:-2], val_per_order[1:-2], labelY="Order($)", title="Mean Ticket over Time", pic_name="")  ##[1:-2] eliminate first and last values as they represent incomplete months
 
     def drawNrOrders(self): 
         orderdate = self.ignoreDays(self.dataset.getDataByName("orders_order_date"))
-        y = np.bincount(self.monthToInt(orderdate.reshape(-1)))
+        y = np.bincount(self.dateToCount(orderdate.reshape(-1)))
         self.drawLine(np.unique(orderdate)[1:-2], y[1:-2], labelY="Nr. Orders", title="Number of Orders", pic_name="nr_orders")  ##[1:-2] eliminate first and last values as they represent incomplete months
         
     def drawProfit(self):
@@ -31,14 +131,68 @@ class SimpleDrawer:
 
         val_per_order = self.dataset.getDataByName("orders_total_price") #value summed for each order
         profit = val_per_order - freight    #### product cost not available
-        month_of_orders = self.monthToInt(self.ignoreDays(orderdate))
+        month_of_orders = self.dateToCount(self.ignoreDays(orderdate))
         profit = pd.DataFrame(np.concatenate([month_of_orders, profit], axis=1)).groupby(0).sum().to_numpy() #total profit per month
         months_to_plot = np.unique(self.ignoreDays(orderdate))
         self.drawLine(months_to_plot[1:-2], profit[1:-2], labelY="Profit", title="Profit (without product cost)", pic_name="profit") ##[1:-2] eliminate first and last values as they represent incomplete months
 
-    def drawLine(self, x, y, labelX="Time", labelY="value", title="", pic_name=""):
+    def drawTBO(self):
+        tbo = self.dataset.getDataByName("utils_tbo")
+        print(np.mean(tbo))
+        tbo_date = self.dataset.getDataByName("utils_tbo_dates")
+        tbo_month = self.ignoreDays(tbo_date)
+        tbo = pd.DataFrame(np.concatenate([self.dateToCount(tbo_month), tbo], axis=1)).groupby(0).mean().to_numpy()
+        self.drawLine(np.unique(tbo_month), tbo, labelY="time between orders (days)", title="Mean Time between Orders", pic_name="tbo_time")  ##[1:-2] eliminate first and last values as they represent incomplete months
+        
+    def drawTTS(self):
+        abs_time = self.dataset.getDataByName("utils_tts")
+        relative_time = self.dataset.getDataByName("utils_tts_relative")
+        month = self.ignoreDays(self.dataset.getDataByName("utils_tts_dates"))
+        abs_time = pd.DataFrame(np.concatenate([self.dateToCount(month), abs_time], axis=1)).groupby(0).mean().to_numpy()
+        relative_time = pd.DataFrame(np.concatenate([self.dateToCount(month), relative_time], axis=1)).groupby(0).mean().to_numpy()
+        y = np.concatenate([abs_time, relative_time], axis=1)
+        self.drawLine(np.unique(month)[1: -2], y[1: -2], labelY="time to Ship", title="Mean Time to Ship Orders(absolute and Relative)", pic_name="ship_time", labels=["TTS(days)", "TTS / total time (%)"])  ##[1:-2] eliminate first and last values as they represent incomplete months
+        
+    def drawChurn(self):
+        churn = self.ignoreDays(self.dataset.getDataByName("customers_churn"))
+        first = self.ignoreDays(self.dataset.getDataByName("customers_first_order"))
+        month = np.unique(self.ignoreDays(self.dataset.getDataByName("orders_order_date")))[1:-2]
+        freq = np.empty([month.shape[0], 2])
+        for i in range(month.shape[0]):
+            freq[i, 0] = np.bincount(churn!=month[i])[0]
+            freq[i, 1] = np.bincount(first!=month[i])[0]
+        
+        delay = self.dataset.getDataByName("utils_tts_delay")
+        del_month = self.ignoreDays(self.dataset.getDataByName("utils_tts_dates"))
+        delay = pd.DataFrame(np.concatenate([self.dateToCount(del_month), delay], axis=1)).groupby(0).sum().to_numpy()
+        freq = np.concatenate([freq, delay[1:-2]], axis=1)
+        
+        self.drawLine(month, freq, labelY="Customers", title="Churns According to buying frequency", pic_name="churn_time", labels=["Churn", "New Customers", "Delays to Send"])
+
+    def drawHistogram(self, array, labelX="interval", labelY="frequency", title="", pic_name=""):
+        y, x = np.histogram(array, bins=20)
+        print(y)
+        print(np.sum(y))
         plt.figure(figsize=(10,6))
-        plt.plot(x, y)
+        plt.hist(x[:-1], x, weights=y)
+        plt.xlabel(labelX)
+        plt.ylabel(labelY)
+        plt.title(title)
+        plt.grid(True)
+        plt.tight_layout()
+        if(pic_name != ""):
+            plt.savefig(path.join("./charts", pic_name + ".png"))
+        plt.show()
+
+
+    def drawLine(self, x, y, labelX="Time", labelY="value", title="", pic_name="", labels=None):
+        plt.figure(figsize=(10,6))
+        if(labels == None):
+            plt.plot(x, y)
+        else:
+            for i in range(len(labels)):
+                plt.plot(x, y[:,i], label=labels[i])
+            plt.legend(loc="upper right")
         plt.xlabel(labelX)
         plt.ylabel(labelY)
         plt.title(title)
@@ -56,9 +210,13 @@ class SimpleDrawer:
 
         return (func(x)).astype('datetime64')
     
-    def monthToInt(self, array):
-        x = array - np.min(array)
+    def dateToCount(*args):
+        array = args[1]
+        if(len(args)==2):
+            reference = args[1][0]
+        else:
+            reference = args[2]
+        x = array - reference
         return x.astype('int32')
-
 
 
