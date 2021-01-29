93c5731a3668dc7b2562db19c2faf2ce31761a49
eng-Marcio <marcio37nandi@gmail.com>
2021-01-28 23:11:16 -0300

    Finish editing analysis program.

--- simple_drawer.py
+++ simple_drawer.py
@@ -7,20 +7,22 @@ import numpy as np
 class SimpleDrawer:
     def __init__(self, dataset):
         self.dataset = dataset
-        # self.drawMeanTicket()
-        # self.drawNrOrders()
-        # self.drawProfit()
-        # self.drawTBO()
-        # self.drawTTS()
-        # self.drawChurn()
+        self.drawMeanTicket()
+        self.drawNrOrders()
+        self.drawProfit()
+        self.drawTBO()
+        self.drawTTS()
+        self.drawChurn()
 
-        # self.drawHistogram(self.dataset.getDataByName("utils_tbo"), title="Histogram TBO", pic_name="hist_tbo")
-        # self.drawHistogram(self.dataset.getDataByName("utils_tts"), title="Histogram TTS", pic_name="hist_tts")
-        # self.drawHistogram(self.dataset.getDataByName("customers_tbo"), title="Histogram TBO-Cliente", pic_name="hist_client_tbo")
-        # self.drawHistogram(self.dataset.getDataByName("orders_total_price"), title="Histogram Ticket", pic_name="hist_price")
+        self.drawHistogram(self.dataset.getDataByName("utils_tbo"), title="Histogram TBO", pic_name="")
+        self.drawHistogram(self.dataset.getDataByName("utils_tts"), title="Histogram TTS", pic_name="")
+        self.drawHistogram(self.dataset.getDataByName("customers_tbo"), title="Histogram TBO-Cliente", pic_name="")
+        self.drawHistogram(self.dataset.getDataByName("orders_total_price"), title="Histogram Ticket", pic_name="")
+        self.drawHistogram(self.dataset.getDataByName("customers_avg_order"), title="Average Order per Customer", pic_name="")
 
-        #self.drawTopProducts()
+        self.drawTopProducts()
         self.draw_category()
+        self.pearsonCorrelation()
 
     def draw_category(self):
         product = self.dataset.getDataByName("order_details_product_id")
@@ -88,27 +90,6 @@ class SimpleDrawer:
 
         self.drawColumn(labels_q,quantities)
         self.drawColumn(labels_v,values)
-
-
-    def drawColumn(self, labels, values, labelX="interval", labelY="frequency", title="", pic_name=""):
-        y_pos = np.arange(len(labels))
-
-        plt.bar(y_pos, values, align='center', alpha=0.5)
-        plt.xticks(y_pos, labels, rotation=90)
-        plt.ylabel(labelY)
-        plt.title(title)
-        plt.gcf().subplots_adjust(bottom=0.5)
-        if(pic_name != ""):
-            plt.savefig(path.join("./charts", pic_name + ".png"))
-        plt.show()
-
-    def drawPie(self, labels, sizes):
-
-        fig1, ax1 = plt.subplots()
-        ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
-                shadow=True, startangle=90)
-        ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-        plt.show()
         
     
     def drawMeanTicket(self):
@@ -117,8 +98,9 @@ class SimpleDrawer:
        
         month_of_orders = self.dateToCount(self.ignoreDays(orderdate))
         val_per_order = pd.DataFrame(np.concatenate([month_of_orders, val_per_order], axis=1)).groupby(0).mean().to_numpy() #mean ticket per month
+        print(np.mean(val_per_order))        
         date_to_plot = np.unique(self.ignoreDays(orderdate))
-        self.drawLine(date_to_plot[1:-2], val_per_order[1:-2], labelY="Order($)", title="Mean Ticket over Time", pic_name="")  ##[1:-2] eliminate first and last values as they represent incomplete months
+        self.drawLine(date_to_plot[1:-2], val_per_order[1:-2], labelY="Order($)", title="Mean Ticket over Time", pic_name="mean_ticket")  ##[1:-2] eliminate first and last values as they represent incomplete months
 
     def drawNrOrders(self): 
         orderdate = self.ignoreDays(self.dataset.getDataByName("orders_order_date"))
@@ -169,7 +151,47 @@ class SimpleDrawer:
         
         self.drawLine(month, freq, labelY="Customers", title="Churns According to buying frequency", pic_name="churn_time", labels=["Churn", "New Customers", "Delays to Send"])
 
+    
+    def pearsonCorrelation(self):
+        churn = self.dataset.getDataByName("customers_churn_rate").reshape(-1)
+        ticket = self.dataset.getDataByName("customers_avg_order").reshape(-1)
+        delta_ticket = self.dataset.getDataByName("customers_delta_order").reshape(-1)
+        tts = self.dataset.getDataByName("customers_tts").reshape(-1)
+        tbo = self.dataset.getDataByName("customers_tbo").reshape(-1)
+        delay = self.dataset.getDataByName("customers_delay_rate").reshape(-1)
+
+        corrs = np.round(np.corrcoef([churn, ticket, delta_ticket, tts, tbo, delay]), decimals=2)
+        
+        labels = ["churn rate", "average Purchase Value", "delta Purchase Value", "Time to Send", "Time between orders", "delay rate"]
+
+        fig, ax = plt.subplots()
+        im = ax.imshow(corrs)
+
+        # We want to show all ticks...
+        ax.set_xticks(np.arange(len(labels)))
+        ax.set_yticks(np.arange(len(labels)))
+        # ... and label them with the respective list entries
+        ax.set_xticklabels(labels)
+        ax.set_yticklabels(labels)
+
+        # Rotate the tick labels and set their alignment.
+        plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+                rotation_mode="anchor")
+
+        # Loop over data dimensions and create text annotations.
+        for i in range(len(labels)):
+            for j in range(len(labels)):
+                text = ax.text(j, i, corrs[i, j],
+                            ha="center", va="center", color="w")
+
+        bottom, top = ax.get_ylim()
+        ax.set_ylim(bottom + 0.5, top - 0.5)
+        ax.set_title("Pearson Correlation Between Customer variables")
+        fig.tight_layout()
+        plt.show()
+    
     def drawHistogram(self, array, labelX="interval", labelY="frequency", title="", pic_name=""):
+        print(np.mean(array))
         y, x = np.histogram(array, bins=20)
         print(y)
         print(np.sum(y))
@@ -200,6 +222,26 @@ class SimpleDrawer:
         plt.tight_layout()
         if(pic_name != ""):
             plt.savefig(path.join("./charts", pic_name + ".png"))
+        plt.show()
+
+    def drawColumn(self, labels, values, labelX="interval", labelY="frequency", title="", pic_name=""):
+        y_pos = np.arange(len(labels))
+
+        plt.bar(y_pos, values, align='center', alpha=0.5)
+        plt.xticks(y_pos, labels, rotation=90)
+        plt.ylabel(labelY)
+        plt.title(title)
+        plt.gcf().subplots_adjust(bottom=0.5)
+        if(pic_name != ""):
+            plt.savefig(path.join("./charts", pic_name + ".png"))
+        plt.show()
+
+    def drawPie(self, labels, sizes):
+
+        fig1, ax1 = plt.subplots()
+        ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
+                shadow=True, startangle=90)
+        ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
         plt.show()
 
     def ignoreDays(self, array):
